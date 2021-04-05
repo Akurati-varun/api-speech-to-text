184d874211416924b4daeb26c726f0903011a735
Akurati Varun <akurativarun@gmail.com>
2021-04-05 11:40:39 +0530

    Code to connect ibmcloud

--- ibm_cloud.py
+++ ibm_cloud.py
@@ -1,6 +1,31 @@
-# import necessary files
+import time 
+import sys 
+import ibmiotf.application 
+import ibmiotf.device 
 
 def processTextCommand(textCommand):
+    
     # Implement cloud command functionality here
+    organization = "pn9oxd" 
+
+    deviceType = "Varun" 
+
+    deviceId = "24062810" 
+
+    authMethod = "token" 
+
+    authToken = "&PPfTQV!s8c)996eZ)" 
+    deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken,"clean-session":True} 
+        
+    deviceCli = ibmiotf.device.Client(deviceOptions)
+    #connect to the cloud
+    deviceCli.connect() 
+        
+    data1={"spoken":textcommand}
+    #publish the event
+    deviceCli.publishEvent(event="command",msgFormat="json",data=data1)
+    time.sleep(0.5)
+    #dissconect from cloud
+    deviceCli.disconnect()   
     pass
     
